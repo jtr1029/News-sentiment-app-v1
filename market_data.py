6a5628a8a713b36623842d5a6ba73600fb198696
jtr1029 <junzhou.wen@ehl.ch>
2025-04-08 15:48:29 +0200

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -4,6 +4,11 @@ import pandas as pd
 def get_market_data(ticker, start, end):
     data = yf.download(ticker, start=start, end=end)
     data.reset_index(inplace=True)
-    data = data[['Date', 'Close']]
-    data.rename(columns={'Close': 'market_close'}, inplace=True)
+
+    # Flatten column index if MultiIndex exists
+    if isinstance(data.columns, pd.MultiIndex):
+        data.columns = ['_'.join(col).strip('_') for col in data.columns.values]
+
+    data = data[['Date', f'Close_{ticker}']]
+    data.rename(columns={f'Close_{ticker}': 'market_close'}, inplace=True)
     return data
