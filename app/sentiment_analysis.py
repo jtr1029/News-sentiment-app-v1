6745cd6b69b958266e1d422cadc450ca6562fcb7
jtr1029 <junzhou.wen@ehl.ch>
2025-04-08 15:09:23 +0200

    Update sentiment_analysis.py

--- app/sentiment_analysis.py
+++ app/sentiment_analysis.py
@@ -1,15 +1,10 @@
 import pandas as pd
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import nltk
-nltk.download('vader_lexicon')
-
-analyzer = SentimentIntensityAnalyzer()
+from textblob import TextBlob
 
 def score_sentiment(text):
     if not isinstance(text, str):
         return 0.0
-    score = analyzer.polarity_scores(text)
-    return score['compound']
+    return TextBlob(text).sentiment.polarity  # Returns value between -1.0 and 1.0
 
 def process_news_dataframe(df):
     df['sentiment'] = df['text'].apply(score_sentiment)
