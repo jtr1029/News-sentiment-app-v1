089ddce2923e16d50367a60ad00c54367cb8f672
jtr1029 <junzhou.wen@ehl.ch>
2025-04-08 15:44:54 +0200

    Update aggregation.py

--- aggregation.py
+++ aggregation.py
@@ -4,5 +4,5 @@ def aggregate_sentiment(df, time_col='date'):
     df[time_col] = pd.to_datetime(df[time_col])
     result = df.groupby(df[time_col].dt.date)['sentiment'].mean().reset_index()
     result.rename(columns={result.columns[0]: 'Date'}, inplace=True)
+    result['Date'] = pd.to_datetime(result['Date'])  # Ensure datetime dtype
     return result
-
