2c83f7e67046a8b7c2c29b3d2a7f12a480c83f20
jtr1029 <junzhou.wen@ehl.ch>
2025-04-08 16:09:51 +0200

    Create news_fetcher.py

---
+++ news_fetcher.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("NEWS_API_KEY")
+
+def fetch_news(ticker, from_date, to_date, max_articles=100):
+    query = f'"{ticker}"'
+    url = f"https://newsapi.org/v2/everything?q={query}&from={from_date}&to={to_date}&language=en&sortBy=publishedAt&pageSize=100&apiKey={API_KEY}"
+
+    response = requests.get(url)
+    articles = response.json().get("articles", [])[:max_articles]
+
+    data = []
+    for article in articles:
+        data.append({
+            "date": article["publishedAt"][:10],
+            "text": article["title"]
+        })
+
+    return pd.DataFrame(data)
