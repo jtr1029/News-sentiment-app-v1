d99389825604d55a6ffe0a5a6db519c53f7e963a
jtr1029 <junzhou.wen@ehl.ch>
2025-04-08 15:37:11 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,10 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def merge_data(sentiment_df, market_df):
-    sentiment_df['Date'] = pd.to_datetime(sentiment_df['date'])
+    sentiment_df['Date'] = pd.to_datetime(sentiment_df['Date'])  # Already renamed in aggregation
     market_df['Date'] = pd.to_datetime(market_df['Date'])
     merged = pd.merge(sentiment_df, market_df, on='Date', how='inner')
     return merged
+
 
 def plot_comparison(merged_df):
     plt.figure(figsize=(10,5))
