610375257caff85fc1d866e5bbb6653a520574b7
jtr1029 <junzhou.wen@ehl.ch>
2025-04-08 15:20:43 +0200

    Delete app directory

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-NEWS_DATA_PATH = "data/raw_news/"
-SENTIMENT_DATA_PATH = "data/sentiment_scores/"
-MARKET_DATA_PATH = "data/stock_prices/"
-
-DEFAULT_TICKER = "AAPL"
-DEFAULT_DATE_RANGE = ("2024-01-01", "2024-12-31")
